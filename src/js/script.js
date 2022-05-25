c4e88742c2946728695817bb27028d095ecb7075
idkngrid <ingrid.camargopr@gmail.com>
2022-05-25 20:46:18 -0300

    add mobile menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,8 @@
 const btnMobile = document.querySelector('#btnMobile');
 
 btnMobile.addEventListener('click', () => {
-    btnMobile.classList.toggle('open');
+    const header = document.querySelector('#header');
+    const overlay = document.querySelector('#overlay');
+    header.classList.toggle('open');
+    overlay.classList.toggle('fade-in');
 });
