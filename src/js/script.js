bdafcf15ef3127de20b4e5a9013e37d6036ffaf8
idkngrid <ingrid.camargopr@gmail.com>
2022-05-26 15:50:37 -0300

    update mobile menu

--- src/js/script.js
+++ src/js/script.js
@@ -4,5 +4,6 @@ btnMobile.addEventListener('click', () => {
     const header = document.querySelector('#header');
     const overlay = document.querySelector('#overlay');
     header.classList.toggle('open');
-    overlay.classList.toggle('fade-in');
+    overlay.classList.toggle('fade-in')
+    overlay.classList.toggle('fade-out', !overlay.classList.contains('fade-in'));
 });
