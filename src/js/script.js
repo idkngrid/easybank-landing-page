d4988fb6a554efc40e3928ecb77ee9da480b7001
idkngrid <ingrid.camargopr@gmail.com>
2022-05-27 16:14:02 -0300

    add hero section

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,12 @@
 const btnMobile = document.querySelector('#btnMobile');
+const body = document.querySelector('body');
 
 btnMobile.addEventListener('click', () => {
     const header = document.querySelector('#header');
     const overlay = document.querySelector('#overlay');
+
     header.classList.toggle('open');
-    overlay.classList.toggle('fade-in')
+    body.classList.toggle('noscroll');
+    overlay.classList.toggle('fade-in');
     overlay.classList.toggle('fade-out', !overlay.classList.contains('fade-in'));
 });
